af197f9e13a353bd7584691fef8c4f482b3a5eff
Jon Zarate <jjzarate215@gmail.com>
2023-06-26 16:45:35 -0700

    Using Fido to download data from JSOC

--- May2023_Graphs.py
+++ May2023_Graphs.py
@@ -7,15 +7,24 @@
 #
 
 import sunpy
-from sunpy.net import jsoc
-from sunpy.net import attrs as a 
+from sunpy.net import Fido
+from sunpy.net import attrs as a
 
-jsoc_client = jsoc.JSOCClient()
-response = jsoc_client.search(
-    a.Time("2023.05.01_00:00:00_TAI", "2023.05.31_12:00:00_TAI"),
-    a.jsoc.Series("hmi.td_fsi_12h"),
-    a.jsoc.Notify("jjzarate215@gmail.com")
-    )
-print(response)
+jsoc_email = "jjzarate215@berkeley.edu"
+
+
+query = Fido.search(
+    a.Time('2023.05.01_00:00:00_TAI', '2023.05.31_12:00:00_TAI'),
+    a.jsoc.Series('hmi.td_fsi_12h'),
+    a.jsoc.Notify(jsoc_email)
+)
+
+print(query)
+directory = 'C:\\Users\\Jjzar\\OneDrive\\Documents\\ASSURE\\ASSURE 2023\\Exploring the Unseen Sun\\Far-SideImagingData\\May2023'
+download_fsi = Fido.fetch(query, path = directory)
+
+#########################################################################
+
+
 
 
